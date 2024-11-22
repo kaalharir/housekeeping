e043c63c284bd1fe80d2422738a3e3d2545838d9
kaal <kaalharir@iut-dhaka.edu>
2024-11-22 19:14:12 +0300

    CSS

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,3 +1,18 @@
+// export default function Main() {
+//     return <h1>Main goes here</h1>;
+// }
 export default function Main() {
-    return <h1>Main goes here</h1>;
+    return (
+        <main>
+            <h1>Fun facts about React</h1>
+            <ul className="facts-list">
+                <li>Was first released in 2013</li>
+                <li>Kaal is learning 2024 after long forgetten memory</li>
+                <li>Was orginally created by Jordan Walke</li>
+                <li>Has well over 200k starts on Github</li>
+                <li>Is maintained by Meta formally known Facebook</li>
+                <li>Powers thousands of enterprise apps, including mobile apps and desktop application</li>
+            </ul>
+        </main>
+    );
 }
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,3 +1,13 @@
-export default function Navbar() {
-    return <h1>Navbar goes here</h1>;
+// export default function Navbar() {
+//     return <h1>Navbar goes here</h1>;
+// }
+export default function Navbar () {
+    return (
+        <header>
+            <nav>
+                <img src="src/assets/logo.jfif" />
+                <span>ReactFacts</span>
+            </nav>
+        </header>
+    )
 }
