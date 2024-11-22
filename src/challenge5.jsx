351394fdfb4866089c7e05e368b2481578bcfbf0
kaal <kaalharir@iut-dhaka.edu>
2024-11-22 17:51:30 +0300

    project template

--- src/challenge5.jsx
+++ src/challenge5.jsx
@@ -4,19 +4,6 @@ const root = createRoot(document.getElementById("root"))
 import Header from "./header"
 import MainContent from "./MainContent";
 import Footer from "./Footer";
-// const root = createRoot(document.getElementById("root"))
-
-/** Mini Challenge:
- * 
- * Move the 'header' element from the Page component into
- * its own component called "Header"
- * 
- * Then render an instance of the Header component inside
- * the Page component where the 'header' used to be. 
- * @returns 
- */
-
-
 
 function Page() {
     return (
